9d1321976d9ca92dacdbf09a693a6e0b4f16fe37
Olga <politykoolga@outlook.com>
2022-09-23 20:48:26 +0300

    Second commit

---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Введите номер строки: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите номер столбца: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [5,5];
+FillArray(numbers);
+
+if (a > numbers.GetLength(0) || b > numbers.GetLength(1))
+{
+    Console.WriteLine("Элемента нет!");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {a} строки и {b} столбца: {numbers[a-1,b-1]}");
+}
+
+PrintArray(numbers);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(-100, 100)/10;
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
